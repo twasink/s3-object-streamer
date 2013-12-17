63702b229be840794b0133b711547cf160982066
Robert Watkins <robertdw@twasink.net>
2013-12-17 22:25:18 +1000

    First post! Included a test that shows how "happy case" streaming works, prior to making it a sad case

--- README.md
+++ README.md
@@ -2,3 +2,45 @@ s3-object-streamer
 ==================
 
 A wrapper around S3Objects that makes streaming and working with S3Objects, and dealing with issues like the underlying HTTPS connection being closed, easier.
+
+Overview
+========
+
+S3 is a fantastic service for storing files. But if you want to store large files (say, batch data) which you then want to process in chunks, it's not so great. S3 Objects are retrieved via HTTPS, which will timeout if you take too long to read it.
+
+It would be great if the AWS SDK could hide that from you - but it doesn't. It _does_ give you the ability to start a download from a particular location in the file (using the HTTP Range header), though. And that's where this project comes in.
+
+This class is an extension to the AWS SDK for Java. It provides a utility class that, given a  handle to a S3 Object, will give you an InputStream that will transparently recover from timeouts. This means you can then write code like this:
+
+    S3Object myS3Object = ...
+    try(BufferedInputStream myStream = new S3ObjectStreamer(myS3Object)) {
+        ... read some data
+        ... do something that might take a long time with that data
+    } catch (IOException e) {
+        ... do what you need to do on an IOException.
+    }
+
+And you don't have to worry about server or client-side timeouts.
+
+Caveats
+=======
+
+* You _do_ have to worry about the data being changed underneath you. The `S3ObjectStreamer` uses the etag returned on the first request to ensure that subsequent blocks of data are consistent. If the resource has changed, you will get an IOException. Same goes if it's deleted while being processed, obviously
+* The S3ObjectStreamer is itself a buffered stream. The default buffer size is 1MB, which you can tune as needed (up to MAX_INT in size). You will obviously need to have enough memory available for the buffer.
+* If there are IOExceptions in populating the buffer, the S3ObjectStreamer will simply pass it up - the only error scenario this attempts to solve is the underlying HTTPS timeout! You can configure the underlying S3Client to do things such as transparently retry in the event of a transient failure.
+
+Credits
+=====
+
+This work wouldn't be possible without Amazon's Web Services, or their Java SDK.
+
+This project was developed by [Robert Watkins](http://twasink.net) (robertdw@twasink.net), in his own time, in part to address an issue observed whilst working for the [Julius Kruttschnitt Mining Research Centre](http://jkmrc.com.au), on the [IES Project](http://www.crcore.org.au/integrated-extraction-simulator.html) - a project co-sponsored by [CRC-ORE](http://www.crcore.org.au).
+
+This project is copyright 2013 to Robert Watkins.
+
+License
+=======
+
+This code is licensed under the MIT License. See the included LICENSE file for details. In general, that means you can do pretty much whatever you want, as long as you keep the license file and don't try to claim your own copyright over it.
+
+Forking of the code, or incorporation into your own code base, is both permitted and strongly encouraged.
---
+++ src/test/java/net/twasink/s3object/EndToEndTest.java
@@ -0,0 +1,156 @@
+package net.twasink.s3object;
+
+import static org.hamcrest.Matchers.array;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.collection.IsArray;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.regions.RegionUtils;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+/**
+ * An example end-to-end test that ensure that the S3 Object Streamer works as advertised. This test first uploads a
+ * medium-size object (4MB of generated data) to S3, and then downloads it. The resulting data is then processed in 
+ * blocks of 512MB, with a 1 minute enforced wait in between.
+ * 
+ * In order to run this test, you will need to specify a number of environment properties. These are:
+ * * AWS_ACCESS_KEY_ID
+ * * AWS_SECRET_KEY
+ * * AWS_REGION - defaults to 'us-east-1'
+ * * S3_BUCKET
+ * 
+ * Alternatively, these can be provided as system properties:
+ * * aws.accessKeyId
+ * * aws.secretKey
+ * * aws.region - defaults to 'us-east-1'
+ * * s3bucket
+ * 
+ * Your AWS account will obviously need to be able to write to and read from the nominated bucket.
+ * 
+ * A random file name will be used (using a UUID); the test _will_ try to delete the file afterwards, of course.
+ */
+public class EndToEndTest {
+    private static final Logger LOG = Logger.getLogger("EndToEndTest");
+    
+    private AmazonS3Client s3Client;
+    private String region;
+    private String bucketName;
+    private String objectId;
+
+    @Before
+    public void setupS3Client() {
+        this.s3Client = new AmazonS3Client();
+    }
+    
+    @Before
+    public void determineRegion() {
+        this.region = System.getenv("AWS_REGION");
+        if (this.region == null || this.region.length() == 0) {
+            this.region = System.getProperty("aws.region", "us-east-1");
+        }
+    }
+    
+    @Before
+    public void determineBucketName() {
+        this.bucketName = System.getenv("S3_BUCKET");
+        if (this.bucketName == null || this.bucketName.length() == 0) {
+            this.bucketName = System.getProperty("s3Bucket");
+        }
+        Assume.assumeThat("You need to specify which S3 Bucket to use", this.bucketName, not(isEmptyOrNullString()));
+    }
+    
+    @After
+    public void cleanupTestData() {
+        if (this.s3Client != null) {
+            LOG.info("Deleting file " + this.objectId + " from " + this.bucketName);
+            this.s3Client.deleteObject(this.bucketName, this.objectId);
+            LOG.info("File " + this.objectId + " deleted from " + this.bucketName);
+        }
+    }
+    
+    @Test
+    public void test() throws IOException {
+        this.s3Client.setRegion(RegionUtils.getRegion(this.region));
+
+        verifyS3BucketExists();
+        
+        byte[] data = createRandomData();
+
+        this.objectId = UUID.randomUUID().toString();
+
+        uploadDataToS3(data);
+        
+        S3Object object = s3Client.getObject(this.bucketName, this.objectId);
+        try(S3ObjectInputStream dataStream = object.getObjectContent()) {
+            BufferedInputStream bufferedStream = new BufferedInputStream(dataStream, 1024 * 1024);
+            for (int i = 0; i < 8; i++) {
+                int bytesToRead = 512 * 1024;
+                byte[] bufferedData = new byte[bytesToRead];
+                
+                LOG.info("Reading block " + i + " of object " + this.objectId + " from " + this.bucketName);
+                
+                int bytesRead = 0;
+                while (bytesRead < bytesToRead) {
+                    bytesRead += bufferedStream.read(bufferedData, bytesRead, bytesToRead - bytesRead);
+                }
+                
+                LOG.info("Read block " + i + " of object " + this.objectId + " from " + this.bucketName);
+                
+                byte[] targetData = Arrays.copyOfRange(data, i * bytesToRead, (i + 1) * bytesToRead);
+                Assert.assertArrayEquals("Data is not equal for block " + i, targetData, bufferedData);
+            }
+        }
+    }
+
+
+    private void uploadDataToS3(byte[] data) {
+        LOG.info("Uploading file " + this.objectId + " to bucket" + this.bucketName);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentEncoding("application/octet-stream");
+        metadata.setContentLength(data.length);
+        
+        PutObjectRequest putRequest = new PutObjectRequest(this.bucketName, objectId, new ByteArrayInputStream(data), metadata);
+        s3Client.putObject(putRequest);
+        LOG.info("File " + this.objectId + " uploaded to bucket" + this.bucketName);
+    }
+
+    private byte[] createRandomData() {
+        byte[] data = new byte[4 * 1024 * 1024];
+        new Random().nextBytes(data);
+        return data;
+    }
+
+    private void verifyS3BucketExists() {
+        LOG.info("Verifying that bucket " + this.bucketName + " exists");
+        Assume.assumeTrue("The S3 bucket must exist", this.s3Client.doesBucketExist(this.bucketName));
+        LOG.info("Bucket " + this.bucketName + " does exist");
+    }
+
+}
